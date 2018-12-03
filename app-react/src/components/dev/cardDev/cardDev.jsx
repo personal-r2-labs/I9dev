865b361ff263c6d7df3537fedd9fb71c336f3bdd
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-12-03 17:33:51 -0200

    adding nav and devs components

---
+++ app-react/src/components/dev/cardDev/cardDev.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+const CardDev = () => {
+    return (
+        <section id="cardDev">
+            <div className="div-one">
+                <img src="/images/icons8-monitor-64.png" width="10%" alt="screen"/>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley </p>
+            </div>
+            <div className="div-two">
+                <img src="/images/icons8-monitor-64.png" width="10%" alt="screen"/>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                </p>
+            </div>
+            <div className="div-three">
+                <img src="/images/icons8-monitor-64.png" width="10%" alt="screen"/>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default CardDev;
---
+++ app-react/src/components/navBar/navBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <a className="navbar-brand text-white" href="#">
+        Freelar
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link text-white" href="#">
+              How it works <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link text-white" href="#">
+              About
+            </a>
+          </li>
+        </ul>
+        <span className="navbar-text">
+          <a className="nav-link text-white" href="#">
+            Signup or Login
+          </a>
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
