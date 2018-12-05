ba17d246759deacd91f1568cdf0012282afebcd1
Ronaldo Rodrigues da Silva <neolondres@outlook.com>
2018-12-05 13:51:59 -0200

    first commit

--- api-server/routes/emp-routes.js
+++ api-server/routes/emp-routes.js
@@ -1,23 +1,2 @@
 import mongoose, { model } from 'mongoose';
 
-const { Schema } = mongoose;
-
-const empSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  linkedinID: String,
-  linkedinURL: String,
-  linkedinToken: String,
-  facebookID: String,
-  facebookToken: String,
-  description: String,
-  photoPath: String
-},
-{
-  timestamps: true
-});
-
-const Enterprising = model('Enterprising', empSchema);
-
-export default Enterprising;
