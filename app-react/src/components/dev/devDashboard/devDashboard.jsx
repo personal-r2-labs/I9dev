a804d8a89c24b106b04f1be81b8d426fc6b101bb
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-12-07 15:28:07 -0200

    first step to create a live edited form

--- app-react/src/components/dev/devDashboard/devDashboard.jsx
+++ app-react/src/components/dev/devDashboard/devDashboard.jsx
@@ -7,8 +7,23 @@ class DevDashBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      devProfile: DevData
+      devProfile: DevData,
+      isEditing: false,
+      initialText: 'Edite aqui'
     };
+  }
+
+  handleEditField(event) {
+    event.preventDefault();
+    console.log(event.target.textContent);
+    this.setState({ isEditing: true, initialText: event.target.textContent });
+    console.log('editfield', this.state.initialText);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ initialText: event.target.value });
+    console.log('Change', this.state.initialText);
   }
 
   render() {
@@ -21,60 +36,67 @@ class DevDashBoard extends Component {
       skills,
       description
     } = this.state.devProfile[0];
+
+    let field;
+
+    if(this.state.isEditing){
+      field = <input type="text" value={this.state.initialText} onChange={event => this.handleChange(event)} />;
+    } else {
+      field = <div onClick={event => this.handleEditField(event)} className="content"><p>{this.state.initialText}</p></div>;
+    }
+
     return (
         <section>
           <NavBar1 />
           <div id="dashboard">
-            <div class="ui card shadow" id="profile-div">
-              <div class="image">
+            <div className="ui card shadow" id="profile-div">
+              <div className="image">
                 <img src={photoPath} />
               </div> 
-              <div class="content">
-                <a class="header">Skills</a>
-                <div class="meta">
-                  <span class="date">Full-Stack Web Developer</span>
+              <div className="content">
+                <a className="header">Skills</a>
+                <div className="meta">
+                  <span className="date">Full-Stack Web Developer</span>
                 </div>
-                <div class="description">
+                <div className="description">
                   {skills}
                 </div>
-                <div class="meta">
-                  <span class="date">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                <div className="meta">
+                  <span className="date">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
                 </div>
               </div>
-              <div class="extra content">
+              <div className="extra content">
                 <a>
-                  <i class="window restore outline icon"></i>
+                  <i className="window restore outline icon"></i>
                   22 Projects
                 </a>
               </div>
             </div>
 
-            <div class="ui internally celled grid">
-              <div class="row">         
-                <div class="ten wide column devDashBoard-profile-margin">
+            <div className="ui internally celled grid">
+              <div className="row">
+                <div className="ten wide column devDashBoard-profile-margin">
                   <h1>{name}</h1>
-                  <div class="ui list">
-                    <div class="item">
-                      <i class="users icon"></i>
-                      <div class="content">
-                        Semantic UI
-                      </div>
+                  <div className="ui list">
+                    <div className="item">
+                      <i className="users icon"></i>
+                      {field}
                     </div>
-                    <div class="item">
-                      <i class="marker icon"></i>
-                      <div class="content">
+                    <div className="item">
+                      <i className="marker icon"></i>
+                      <div className="content">
                         New York, NY
                       </div>
                     </div>
-                    <div class="item">
-                      <i class="mail icon"></i>
-                      <div class="content">
+                    <div className="item">
+                      <i className="mail icon"></i>
+                      <div className="content">
                         <a href="mailto:jack@semantic-ui.com">jack@semantic-ui.com</a>
                       </div>
                     </div>
-                    <div class="item">
-                      <i class="linkify icon"></i>
-                      <div class="content">
+                    <div className="item">
+                      <i className="linkify icon"></i>
+                      <div className="content">
                         <a href="http://www.semantic-ui.com">semantic-ui.com</a>
                       </div>
                     </div>
@@ -82,13 +104,13 @@ class DevDashBoard extends Component {
                 </div>
               </div>
 
-              <div class="row">
-                <div class="ten wide column devDashBoard-profile-margin">
+              <div className="row">
+                <div className="ten wide column devDashBoard-profile-margin">
                   <h1>Projetos</h1>
-                  <div class="ui card shadow">
-                    <div class="content">
-                      <div class="header">Projeto Ironhack</div>
-                      <div class="meta">
+                  <div className="ui card shadow">
+                    <div className="content">
+                      <div className="header">Projeto Ironhack</div>
+                      <div className="meta">
                         <p>Freelance</p>
                         <a>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</a>
                       </div>
--- app-react/src/components/navBar/navBar1.jsx
+++ app-react/src/components/navBar/navBar1.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const NavBar1 = () => {
     return (
-      <div class="ui inverted menu">
-        <a class="active header item">
+      <div className="ui inverted menu">
+        <a className="active header item">
           i9Dev
         </a>
-        <a class=" header item">
+        <a className=" header item">
           Find project
         </a>
-        <a class=" header item">
+        <a className=" header item">
           Profile
         </a>
-        <a class="header item">
+        <a className="header item">
           About us
         </a>
-        <div class="right menu">
-          <a class="item header">profile name</a>
-          <a class="item">Logout</a>
+        <div className="right menu">
+          <a className="item header">profile name</a>
+          <a className="item">Logout</a>
         </div>
       </div>
     )
