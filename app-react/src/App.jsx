d4be0593707266f612342f423e5556e9b6c92eb3
Romulo Franca <romulo2franca@gmail.com>
2018-12-07 17:16:06 -0200

    semantic ui installed and add a folder test

--- app-react/src/App.jsx
+++ app-react/src/App.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import DevDashBoard from './components/dev/devDashboard/devDashboard.jsx'
-import Home from './components/home/home'
+import ButtonExampleInverted from './components/test/ButtonExampleInverted'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <ButtonExampleInverted />
       </div>
     );
   }
