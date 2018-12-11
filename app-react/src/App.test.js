84d322a9c1d15d0fc67de0fe7e2cebbd0807cdd6
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-12-11 11:26:32 -0200

    fixed signup form

--- app-react/src/App.test.js
+++ app-react/src/App.test.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import AddProject from './components/dashboard/ProjectDashboard/ProjectAddForm/ProjectAddForm';
+import SignUp from './components/auth/SignUp/SignUp'; 
 
 class AppTest extends Component {
   render() {
     return (
       <div className="App">
-      <AddProject />
+      <SignUp />
       </div>
     );
   }
