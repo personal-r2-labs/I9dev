d456f017031319827cd58a9c4f42dc678fb99e97
Romulo Franca <romulo2franca@gmail.com>
2018-12-12 00:28:48 -0200

    return state from ProjectSearch

--- app-react/src/components/dashboard/ProjectDashboard/ProjectSearch/ProjectSearch.jsx
+++ app-react/src/components/dashboard/ProjectDashboard/ProjectSearch/ProjectSearch.jsx
@@ -1,23 +1,41 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { Search } from 'semantic-ui-react';
+import { Search, Grid, GridColumn } from 'semantic-ui-react';
+import ProjectSearchOption from '../ProjectSearchOption/ProjectSearchOption';
+import axios from 'axios';
 
 export default class ProjectSearch extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
+    this.source = '';
   }
-  
+  componentDidMount() {
+    this.getProjects();
+  }
+
   componentWillMount() {
     this.resetComponent();
   }
 
-  resetComponent = () => {
-    this.setState({ isLoading: false, results: [], value: '' });
-  }
+  getProjects = () => {
+    axios
+      .get(`http://localhost:5000/api/projects`)
+      .then(responseFromApi => {
+        const theProfile = responseFromApi.data;
+        this.source = theProfile;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-  handleResultSelect = (e, { result }) => 
+  resetComponent = () =>
+    this.setState({ isLoading: false, results: [], value: '' });
+
+  handleSearchOption = (e, { option }) => this.setState({ option });
+
+  handleResultSelect = (e, { result }) =>
     this.setState({ value: result[this.state.option] });
 
   handleSearchChange = (e, { value }) => {
@@ -28,24 +46,44 @@ export default class ProjectSearch extends Component {
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
       const isMatch = result => re.test(result[this.state.option]);
+      console.log(this.source);
       this.setState({
         isLoading: false,
-        results: _.filter(this.props.source, isMatch)
+        results: _.filter(this.source, isMatch)
       });
     }, 300);
   };
-  
+
   render() {
-    const { isLoading, value, results } = this.state;
-    return <Search
+    const { isLoading, value, option } = this.state;
+    console.log(option);
+    return (
+      <Grid>
+        <GridColumn width={10}>
+          <Search
             loading={isLoading}
             onResultSelect={this.handleResultSelect}
             onSearchChange={_.debounce(this.handleSearchChange, 500, {
               leading: true
             })}
             value={value}
-            results={results}
+            showNoResults={false}
             {...this.props}
           />
+          <ProjectSearchOption
+            changed={this.handleSearchOption}
+            checked={option}
+          />
+        </GridColumn>
+        {/* <Grid.Column width={10}>
+          <Segment>
+            <Header>State</Header>
+            <pre style={{ overflowX: 'auto' }}>{JSON.stringify(this.state, null, 2)}</pre>
+            <Header>Options</Header>
+            <pre style={{ overflowX: 'auto' }}>{JSON.stringify(this.source, null, 2)}</pre>
+          </Segment>
+        </Grid.Column> */}
+      </Grid>
+    );
   }
 }
